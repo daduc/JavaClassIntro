0767ea60159f9578a812b1dcf21985b2cc074777
daduc <duchdang1@gmail.com>
2019-04-12 11:20:55 -0500

    quiz1

---
+++ src/com.company/JavaQuiz1.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class JavaQuiz1 {
+    /*
+    These are the answers
+    For Basics Java Quiz 1
+    10
+
+    25
+
+    byte myFirstNum=10
+
+
+    public static void main (String [] args)
+
+    true
+
+    yes
+
+    object or instance, class or type, methods or function, inheritance
+
+     Git with help you keep your repository so you can keep your files so you can commit push and pull
+
+      Version control si a system that records changes to a file or set of files over time so that you can recall specific versions later
+
+
+     strings are sequence of characters
+
+    yes
+
+    commit, push pull
+
+    Code comment are used to explain what your trying to do, or can also serve as a reminder to do something
+
+    // when you only write single comment line it wont do anything just a reminder
+
+    /*
+    within this the whole block of comment won't do anything but just a reminder
+
+
+
+    String str1 = "Hello";
+    String str2 = "Hello";
+    String str1 = new "Hello";
+    String str2 = new "Hello";
+
+    CharAT(int index);
+
+    concat(string str);
+
+    equals(Object object);
+
+    toLowerCase();
+
+    toUpperCase();
+
+    Variables arem nothing but reserved memory locations store values.This mean that when you create a variable you reserve some space in memory
+
+    boolean,char,byte,shortint,long,float,double
+
+    int x;
+    float f = 32.5f
+
+    yes
+
+
+
+
+
+     */
+
+
+
+
+
